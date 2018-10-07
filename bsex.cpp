d257a1a8f49b7133407e64230d73ee34c72d2b93
Jussi Laako <jussi@sonarnerd.net>
2018-10-07 12:06:16 +0300

    Add file name parameters for direct file access in typical use case
    Also adds verify method

--- bsex.cpp
+++ bsex.cpp
@@ -79,7 +79,8 @@ static void keygen (const std::string &baseName)
 }
 
 
-static void encrypt (const std::string &signBaseName, const std::string &wrapBaseName)
+static void encrypt (const std::string &signBaseName, const std::string &wrapBaseName,
+    std::ostream &fileOut)
 {
     Botan::AutoSeeded_RNG PRNG;
 
@@ -105,33 +106,33 @@ static void encrypt (const std::string &signBaseName, const std::string &wrapBas
     std::vector<uint8_t> WrappedIV(WrapEncryptor.encrypt(SymIV, PRNG));
     // wrapped key
     u32WrappedSize = static_cast<uint32_t> (WrappedKey.size());
-    std::cout.write(reinterpret_cast<char *> (&u32WrappedSize), sizeof(u32WrappedSize));
-    if (!std::cout.good())
+    fileOut.write(reinterpret_cast<char *> (&u32WrappedSize), sizeof(u32WrappedSize));
+    if (!fileOut.good())
         throw std::range_error("failed to write wrapped key size");
-    std::cout.write(reinterpret_cast<char *> (WrappedKey.data()), WrappedKey.size());
-    if (!std::cout.good())
+    fileOut.write(reinterpret_cast<char *> (WrappedKey.data()), WrappedKey.size());
+    if (!fileOut.good())
         throw std::range_error("failed to write wrapped key");
     // wrapped IV
     u32WrappedSize = static_cast<uint32_t> (WrappedIV.size());
-    std::cout.write(reinterpret_cast<char *> (&u32WrappedSize), sizeof(u32WrappedSize));
-    if (!std::cout.good())
+    fileOut.write(reinterpret_cast<char *> (&u32WrappedSize), sizeof(u32WrappedSize));
+    if (!fileOut.good())
         throw std::range_error("failed to write wrapped iv size");
-    std::cout.write(reinterpret_cast<char *> (WrappedIV.data()), WrappedIV.size());
-    if (!std::cout.good())
+    fileOut.write(reinterpret_cast<char *> (WrappedIV.data()), WrappedIV.size());
+    if (!fileOut.good())
         throw std::range_error("failed to write wrapped iv");
 
     uint32_t u32SigSize;
-    size_t sizeSigPos = std::cout.tellp();
+    size_t sizeSigPos = fileOut.tellp();
     Botan::PK_Signer Signer(*SignKey.get(), PRNG, "SHA-512");
     u32SigSize = Signer.signature_length();
     std::vector<uint8_t> Signature(u32SigSize);
     fprintf(stderr, "sigpos=%lu, sigsize=%u\n", sizeSigPos, u32SigSize);
     // signature placeholder
-    std::cout.write(reinterpret_cast<char *> (&u32SigSize), sizeof(u32SigSize));
-    if (!std::cout.good())
+    fileOut.write(reinterpret_cast<char *> (&u32SigSize), sizeof(u32SigSize));
+    if (!fileOut.good())
         throw std::range_error("failed to write signature size");
-    std::cout.write(reinterpret_cast<char *> (Signature.data()), Signature.size());
-    if (!std::cout.good())
+    fileOut.write(reinterpret_cast<char *> (Signature.data()), Signature.size());
+    if (!fileOut.good())
         throw std::range_error("failed to write signature");
 
     const size_t sizeBlock = 1048576 * 4;  // 4 MiB
@@ -142,22 +143,23 @@ static void encrypt (const std::string &signBaseName, const std::string &wrapBas
         std::cin.read(reinterpret_cast<char *> (CipherBlock.data()), CipherBlock.size());
         Signer.update(CipherBlock.data(), std::cin.gcount());
         SymCipher->cipher(CipherBlock.data(), CipherBlock.data(), std::cin.gcount());
-        std::cout.write(reinterpret_cast<char *> (CipherBlock.data()), std::cin.gcount());
-        if (!std::cout.good())
+        fileOut.write(reinterpret_cast<char *> (CipherBlock.data()), std::cin.gcount());
+        if (!fileOut.good())
             throw std::range_error("failed to write cipher block");
     }
 
     Signature = Signer.signature(PRNG);
     // signature
-    std::cout.seekp(sizeSigPos + sizeof(u32SigSize));
-    std::cout.write(reinterpret_cast<char *> (Signature.data()), Signature.size());
-    if (!std::cout.good())
+    fileOut.seekp(sizeSigPos + sizeof(u32SigSize));
+    fileOut.write(reinterpret_cast<char *> (Signature.data()), Signature.size());
+    if (!fileOut.good())
         throw std::range_error("failed to write signature");
-    std::cout.seekp(0, std::ios_base::end);
+    fileOut.seekp(0, std::ios_base::end);
 }
 
 
-static void decrypt (const std::string &wrapBaseName, const std::string &signBaseName)
+static void decrypt (const std::string &wrapBaseName, const std::string &signBaseName,
+    std::istream &fileIn, bool verify = false)
 {
     Botan::AutoSeeded_RNG PRNG;
 
@@ -173,28 +175,28 @@ static void decrypt (const std::string &wrapBaseName, const std::string &signBas
     std::vector<uint8_t> WrappedIV;
     std::vector<uint8_t> Signature;
     // wrapped key
-    std::cin.read(reinterpret_cast<char *> (&u32WrappedSize), sizeof(u32WrappedSize));
-    if (!std::cin.good())
+    fileIn.read(reinterpret_cast<char *> (&u32WrappedSize), sizeof(u32WrappedSize));
+    if (!fileIn.good())
         throw std::range_error("failed to read wrapped key size");
     WrappedKey.resize(u32WrappedSize);
-    std::cin.read(reinterpret_cast<char *> (WrappedKey.data()), WrappedKey.size());
-    if (!std::cin.good())
+    fileIn.read(reinterpret_cast<char *> (WrappedKey.data()), WrappedKey.size());
+    if (!fileIn.good())
         throw std::range_error("failed to read wrapped key");
     // wrapped iv
-    std::cin.read(reinterpret_cast<char *> (&u32WrappedSize), sizeof(u32WrappedSize));
-    if (!std::cin.good())
+    fileIn.read(reinterpret_cast<char *> (&u32WrappedSize), sizeof(u32WrappedSize));
+    if (!fileIn.good())
         throw std::range_error("failed to read wrapped iv size");
     WrappedIV.resize(u32WrappedSize);
-    std::cin.read(reinterpret_cast<char *> (WrappedIV.data()), WrappedIV.size());
-    if (!std::cin.good())
+    fileIn.read(reinterpret_cast<char *> (WrappedIV.data()), WrappedIV.size());
+    if (!fileIn.good())
         throw std::range_error("failed to read wrapped iv");
     // signature
-    std::cin.read(reinterpret_cast<char *> (&u32SigSize), sizeof(u32SigSize));
-    if (!std::cin.good())
+    fileIn.read(reinterpret_cast<char *> (&u32SigSize), sizeof(u32SigSize));
+    if (!fileIn.good())
         throw std::range_error("failed to read signature size");
     Signature.resize(u32SigSize);
-    std::cin.read(reinterpret_cast<char *> (Signature.data()), Signature.size());
-    if (!std::cin.good())
+    fileIn.read(reinterpret_cast<char *> (Signature.data()), Signature.size());
+    if (!fileIn.good())
         throw std::range_error("failed to read signature");
 
     Botan::secure_vector<uint8_t> SymKey(WrapDecryptor.decrypt(WrappedKey));
@@ -210,14 +212,18 @@ static void decrypt (const std::string &wrapBaseName, const std::string &signBas
     Botan::secure_vector<uint8_t> CipherBlock(sizeBlock);
     Botan::PK_Verifier Verifier(*SignKey.get(), "SHA-512");
 
-    while (std::cin.good())
+    while (fileIn.good())
     {
-        std::cin.read(reinterpret_cast<char *> (CipherBlock.data()), CipherBlock.size());
-        SymCipher->cipher(CipherBlock.data(), CipherBlock.data(), std::cin.gcount());
-        Verifier.update(CipherBlock.data(), std::cin.gcount());
-        std::cout.write(reinterpret_cast<char *> (CipherBlock.data()), std::cin.gcount());
-        if (!std::cout.good())
-            throw std::range_error("failed to write cipher block");
+        fileIn.read(reinterpret_cast<char *> (CipherBlock.data()), CipherBlock.size());
+        SymCipher->cipher(CipherBlock.data(), CipherBlock.data(), fileIn.gcount());
+        Verifier.update(CipherBlock.data(), fileIn.gcount());
+        if (!verify)
+        {
+            std::cout.write(reinterpret_cast<char *> (CipherBlock.data()),
+                fileIn.gcount());
+            if (!std::cout.good())
+                throw std::range_error("failed to write cipher block");
+        }
     }
 
     if (Verifier.check_signature(Signature))
@@ -231,8 +237,9 @@ static void print_help (const char *execname)
 {
     std::cerr << execname << " command args..." << std::endl;
     std::cerr << "\tkeygen <basename>" << std::endl;
-    std::cerr << "\tencrypt <own basename> <recipient basename>" << std::endl;
-    std::cerr << "\tdecrypt <own basename> <sender basename>" << std::endl;
+    std::cerr << "\tencrypt <own basename> <recipient basename> [output filename]" << std::endl;
+    std::cerr << "\tdecrypt <own basename> <sender basename> [input filename]" << std::endl;
+    std::cerr << "\tverify <own basename> <sender basename> [input filename]" << std::endl;
 }
 
 
@@ -251,9 +258,29 @@ int main (int argc, char *argv[])
         if (Cmd == "keygen" && argc == 3)
             keygen(std::string(argv[2]));
         else if (Cmd == "encrypt" && argc == 4)
-            encrypt(std::string(argv[2]), std::string(argv[3]));
+            encrypt(std::string(argv[2]), std::string(argv[3]), std::cout);
+        else if (Cmd == "encrypt" && argc == 5)
+        {
+            std::ofstream fileOut(argv[4],
+                std::ios_base::out | std::ios_base::binary | std::ios_base::trunc);
+            encrypt(std::string(argv[2]), std::string(argv[3]), fileOut);
+        }
         else if (Cmd == "decrypt" && argc == 4)
-            decrypt(std::string(argv[2]), std::string(argv[3]));
+            decrypt(std::string(argv[2]), std::string(argv[3]), std::cin);
+        else if (Cmd == "decrypt" && argc == 5)
+        {
+            std::ifstream fileIn(argv[4],
+                std::ios_base::in | std::ios_base::binary);
+            decrypt(std::string(argv[2]), std::string(argv[3]), fileIn);
+        }
+        else if (Cmd == "verify" && argc == 4)
+            decrypt(std::string(argv[2]), std::string(argv[3]), std::cin, true);
+        else if (Cmd == "verify" && argc == 5)
+        {
+            std::ifstream fileIn(argv[4],
+                std::ios_base::in | std::ios_base::binary);
+            decrypt(std::string(argv[2]), std::string(argv[3]), fileIn, true);
+        }
         else
             print_help(argv[0]);
     }
